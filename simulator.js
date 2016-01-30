8d8216466452c20e7318d5b2207b6e92814e43ad
Julia Biella <julia.biella@t-online.de>
2016-01-30 22:22:42 +0100

    make fire settable in the forest

--- simulator.js
+++ simulator.js
@@ -10,30 +10,74 @@
   var CANVAS_WIDTH = parseInt(cs.getPropertyValue('width'), 10),
       CANVAS_HEIGHT = parseInt(cs.getPropertyValue('height'), 10);
 
+  var COLOUR = {
+    TREE_ALIVE: 'rgba(0, 92, 42, 1)',
+    TREE_DEAD: 'rgba(92, 15, 7, 1)',
+    FIRE: 'rgba(248, 53, 42, 1)',
+    GRASS: 'rgba(25, 169, 91, 1)'
+  }
+
   canvas.width = CANVAS_WIDTH;
   canvas.height = CANVAS_HEIGHT;
 
   // assume 5 pixel squares for grid cells
-  var PIXEL_WIDTH = PIXEL_HEIGHT = 5,
+  var PIXEL_WIDTH = PIXEL_HEIGHT = 10,
       context = canvas.getContext('2d');
 
-  function randomValue() {
-    return Math.floor(Math.random() * (255 + 1));
-  }
-
-  function randomColor() {
-    return 'rgba(' + randomValue() + ',' + randomValue() + ',' + randomValue() + ',1)';
-  }
-
-  function drawPixel(x, y) {
-    context.fillStyle = randomColor();
+  function drawPixel(x, y, actor) {
+    var actor = typeof actor === 'function' ? actor() : Math.random() <= density ? Tree() : Grass();
+    context.fillStyle = actor.colour;
     context.fillRect(x, y, PIXEL_WIDTH, PIXEL_HEIGHT);
   }
 
-  for (var i = 0; i <= CANVAS_WIDTH; i += PIXEL_WIDTH) {
+  var wetness = parseInt($('#tree-wetness').val(), 10);
+  function Tree() {
+    return {
+      colour: COLOUR.TREE_ALIVE,
+      behaviour: function() {},
+      wetness: wetness
+    }
+  }
+
+  function Fire() {
+    return {
+      colour: COLOUR.FIRE,
+      behaviour: function() {},
+      lifepoints: 3
+    }
+  }
+
+  function Grass() {
+    return {
+      colour: COLOUR.GRASS,
+      behaviour: function() {}
+    }
+  }
+
+  function drawCanvas() {
+    density = parseInt($('#tree-density').val(), 10);
+    density = density > 100 ? 100 : density;
+    density /= 100;
     for (var k = 0; k < CANVAS_HEIGHT; k += PIXEL_HEIGHT) {
-      drawPixel(i, k);
+      for (var i = 0; i <= CANVAS_WIDTH; i += PIXEL_WIDTH) {
+        drawPixel(i, k);
+      };
     };
-  };
+  }
+
+  function setFire(event) {
+    var x = event.pageX - $(canvas).offset().left,
+        y = event.pageY - $(canvas).offset().top,
+        coordX = Math.floor(x / PIXEL_WIDTH) * PIXEL_WIDTH,
+        coordY = Math.floor(y / PIXEL_HEIGHT) * PIXEL_HEIGHT;
+
+    drawPixel(coordX, coordY, Fire);
+  }
+
+  drawCanvas();
+
+  $('#regenerate').on('click', drawCanvas);
+
+  $(canvas).on('click', setFire);
 
 }(jQuery));
